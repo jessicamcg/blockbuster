879a9e63e4e4c38a34a2eb69ee95b05dbe471802
Jessica Guico <89049391+jessicamcg@users.noreply.github.com>
2022-10-31 15:58:20 -0500

    connection jdbc

--- src/main/java/dao/Connection.java
+++ src/main/java/dao/Connection.java
@@ -1,4 +1,27 @@
 package dao;
 
+import lombok.NoArgsConstructor;
+
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+@NoArgsConstructor
 public class Connection {
+  private String jdbcURL = "jdbc:mysql://localhost:3306/blockbuster?useSSL=false";
+  private String jdbcUsername = "root";
+  private String jdbcPassword = "";
+
+  protected java.sql.Connection getConnection() {
+    java.sql.Connection connection = null;
+    try {
+      Class.forName("com.mysql.cj.jdbc.Driver");//MySQL database version 8.0
+      connection = DriverManager.getConnection(jdbcURL, jdbcUsername, jdbcPassword);
+    } catch (SQLException e) {
+      e.printStackTrace();
+    } catch (ClassNotFoundException e) {
+      e.printStackTrace();
+    }
+    return connection;
+  }
+
 }
